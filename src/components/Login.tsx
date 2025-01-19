0d7e3b1eb4b1ff8209e91ea9dc52a25236d889b5
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2025-01-19 22:02:16 +0545

    added login page

---
+++ src/components/Login.tsx
@@ -0,0 +1,13 @@
+const Login: React.FC = () => {
+  return (
+    <div className="flex items-center justify-center">
+      <div className="w-80 p-3">
+        <div className="py-2">
+          <h2 className="text-center text-lg font-semibold">BEEN</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
