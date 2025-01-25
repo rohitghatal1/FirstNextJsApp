9ef83fb5543e142175a77875f2ba9db5ea32b06d
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2025-01-25 14:05:56 +0545

    added login page

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,25 +1,59 @@
 import { Button, Form, Input } from "antd";
+import { useState } from "react";
+import { FaRegUser } from "react-icons/fa6";
 
 const Login: React.FC = () => {
+  const [loginForm] = Form.useForm();
+  const [isLoginFormOpen, setIsLoginFormOpen] = useState<boolean>(true);
+  const hanldeFormSubmit = async (values: any) => {};
   return (
     <div className="flex items-center justify-center">
-      <div className="w-80 p-3">
-        <div className="py-2">
-          <h2 className="text-center text-lg font-semibold">BEEN</h2>
-
-          <div className="w-full mt-3 px-2">
-            <Form layout="vertical">
-              <Form.Item label="Mobile Number">
-                <Input type="number" />
+      {isLoginFormOpen && (
+        <div className="border p-2 sm:p-4 w-[98%] sm:w-[35rem]">
+          <Form layout="vertical" form={loginForm} onFinish={hanldeFormSubmit}>
+            {/* {!isRegistered && (
+              <Form.Item label="User Name" name="username">
+                <Input size="large" />
               </Form.Item>
+            )} */}
+            <h2 className="font-semibold text-lg text-center py-2 flex items-center gap-1">
+              <FaRegUser /> User Login
+            </h2>
+            <Form.Item
+              label="Mobile Number"
+              name="mobile_number"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your mobile number!",
+                },
+                { len: 10, message: "Mobile number must be 10 digits!" },
+              ]}
+              className="mt-4"
+            >
+              <Input type="number" size="large" />
+            </Form.Item>
 
-              <Form.Item>
-                <Button type="primary" size="large"></Button>
-              </Form.Item>
-            </Form>
-          </div>
+            <Form.Item>
+              <button
+                className="bg-green-600 text-white border border-green-600 px-5 py-2 rounded-md"
+                type="submit"
+              >
+                Get OTP
+              </button>
+            </Form.Item>
+            {/* <Button
+              type="link"
+              className="mt-2"
+              onClick={() => setIsRegistered(!isRegistered)}
+            >
+              {isRegistered
+                ? "Not registered? Register here."
+                : "Already registered? Login here."}
+            </Button> */}
+          </Form>
         </div>
-      </div>
+      )}
     </div>
   );
 };
