42fe09a3ffd8d9856145e5844e067ac3f4aaa58d
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2025-02-13 21:11:26 +0545

    adde about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,19 @@
+const AboutUs = () => {
+  return (
+    <div className="max-w-7xl mx-auto px-6 py-12 min-h-screen">
+      <section className="text-center mb-12">
+        <h1 className="text-4xl font-semibold text-gray-800">
+          Welcome to Been
+        </h1>
+        <p className="mt-4 text-lg text-gray-600">
+          At Been, we envision a sustainable future where every home contributes
+          to a healthier planet. As a platform dedicated to eco-friendly
+          construction and energy-efficient home solutions, we aim to
+          revolutionize how homes are built and lived in.
+        </p>
+      </section>
+    </div>
+  );
+};
+
+export default AboutUs;
